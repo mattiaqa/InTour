41b67135d81fa4efe5e2b02fbbe616e3f5ee1d9c
mattiaqa <888515@stud.unive.it>
2024-06-20 23:45:49 +0200

    release v1.1

--- frontend/lib/utils/constants.dart
+++ frontend/lib/utils/constants.dart
@@ -1,8 +1,1 @@
 String myIP = "161.35.21.216";
-
-//String myIP = "212.132.103.182";
-//String myIP = "192.168.1.87";
-//String myIP = "192.168.1.61";
-//String myIP = "172.20.10.3";
-//String myIP = "192.168.8.110";
-String myIP = "161.35.21.216";
